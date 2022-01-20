2d746777eae398a79f08d036288423bca6c813e9
Marina Merini Valcanaia <mmvalcanaia@gmail.com>
2022-01-20 10:18:20 +0100

    Removing completed task

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,28 +1,29 @@
-import Task from '../models/task.js';
-import * as listController from './list-controller.js';
+import Task from "../models/task.js";
+import * as listController from "./list-controller.js";
 
-function State(){
-    this.Task = new Task();
+function State() {
+  this.Task = new Task();
 
-    this.inputTask = null;
-    this.btnSave = null;
+  this.inputTask = null;
+  this.btnSave = null;
 }
 
 const state = new State();
 
-export function init(){
-    state.inputTask = document.querySelector(".input-task");
-    state.btnSave = document.querySelector(".btn-add-task");
+export function init() {
+  state.inputTask = document.querySelector(".input-task");
+  state.btnSave = document.querySelector(".btn-add-task");
 
-    state.btnSave.addEventListener("click", handleBtnSaveClick);
+  state.btnSave.addEventListener("click", handleBtnSaveClick);
 }
 
-function handleBtnSaveClick(event){
-    event.preventDefault();
-    listController.addNewTaskToScreen(state.inputTask);
-    clearInputTask();
+function handleBtnSaveClick(event) {
+  event.preventDefault();
+
+  listController.addNewTaskToScreen(state.inputTask);
+  clearInputTask();
 }
 
-function clearInputTask(){
-    state.inputTask.value = "";
+function clearInputTask() {
+  state.inputTask.value = "";
 }
--- js/controllers/list-controller.js
+++ js/controllers/list-controller.js
@@ -1,11 +1,14 @@
 function State() {
   this.listContainer = null;
+  this.btnCheckBox = null;
+  this.task = null;
 }
 
 const state = new State();
 
 export function init() {
   state.listContainer = document.querySelector("#to-do-list");
+  state.task = document.querySelectorAll("to-do-item");
 }
 
 export function addNewTaskToScreen(task) {
@@ -13,12 +16,30 @@ export function addNewTaskToScreen(task) {
   const list = addItemtoList(newTask);
 
   state.listContainer.appendChild(list);
+  addEventListenerToEveryCheckbox();
+}
+
+function addEventListenerToEveryCheckbox() {
+  const checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", handleCheckBoxTaskChange);
+  });
 }
 
 function createListItem(task) {
+  const taskCheckBox = document.createElement("input");
+  taskCheckBox.setAttribute("type", "checkbox");
+
+  const label = document.createElement("label");
+  label.innerHTML = task.value;
+
+  taskCheckBox.classList.add("to-do-item-check");
+
   const li = document.createElement("li");
+  li.appendChild(taskCheckBox);
+  li.appendChild(label);
   li.classList.add("to-do-item");
-  li.innerHTML = task.value;
   return li;
 }
 
@@ -39,3 +60,24 @@ function createList() {
   ul.classList.add("to-do-list-ul");
   return ul;
 }
+
+function handleCheckBoxTaskChange(event) {
+  const checkBoxDone = event.target;
+  if (checkBoxDone.checked) {
+    console.log(this);
+    removeTask(this);
+  }
+}
+
+function removeTask(task) {
+  const x = document.querySelector(".to-do-item-check").parentElement;
+  x.remove();
+}
+
+// function addTaskToDoneList(doneTask){
+//   const ulDoneTasks = document.createElement("ul");
+//   ulDoneTasks.classList.add("to-do-item-done");
+//   ulDoneTasks.appendChild(doneTask);
+//   doneTask.classList.add("to-do-item-done");
+//   doneTask.classList.remove("to-do-item");
+// }
